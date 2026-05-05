9218c8489589842958c8bc89041e818a3ee1b286
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-05-05 14:47:44 +0300

    fix: view layout constraint impl
    Fix View layout constraint implementation to support old devices.

--- MovieQuizSE/Presentation/QuizViewController.m
+++ MovieQuizSE/Presentation/QuizViewController.m
@@ -24,48 +24,56 @@
 
 - (void)loadView {
     self.view = [UIView new];
-    self.view.backgroundColor = [UIColor ypWhite];
+    self.view.backgroundColor = [UIColor ypBlack];
     
     questionHeaderLabel = [UILabel new];
     questionHeaderLabel.translatesAutoresizingMaskIntoConstraints = NO;
     questionHeaderLabel.font = [UIFont ysMedium20];
     questionHeaderLabel.text = NSLocalizedString(@"QuestionHeaderTitle", "");
+    questionHeaderLabel.textColor = [UIColor ypWhite];
     questionHeaderLabel.textAlignment = NSTextAlignmentLeft;
-    questionHeaderLabel.backgroundColor = [UIColor ypRed];
     
     questionCounterLabel = [UILabel new];
     questionCounterLabel.translatesAutoresizingMaskIntoConstraints = NO;
     questionCounterLabel.font = [UIFont ysMedium20];
     questionCounterLabel.text = @"1/10";
+    questionCounterLabel.textColor = [UIColor ypWhite];
     questionCounterLabel.textAlignment = NSTextAlignmentRight;
     
     questionRatingLabel = [UILabel new];
     questionRatingLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    questionRatingLabel.font = [UIFont ysBold23];
-    questionRatingLabel.text =  NSLocalizedString(@"QuestionRatingTitle", "");
+    questionRatingLabel.font = [UIFont ysMedium20];
+    questionRatingLabel.text =  [NSLocalizedString(@"QuestionRatingTitle", "") stringByAppendingString:@" 8?"];
+    questionRatingLabel.textColor = [UIColor ypWhite];
     questionRatingLabel.textAlignment = NSTextAlignmentCenter;
     
     acceptButton = [UIButton buttonWithType:UIButtonTypeSystem];
     acceptButton.translatesAutoresizingMaskIntoConstraints = NO;
+    acceptButton.layer.cornerRadius = 15;
     acceptButton.titleLabel.font = [UIFont ysMedium20];
+    [acceptButton setBackgroundColor:[UIColor ypWhite]];
+    [acceptButton setTitleColor:[UIColor ypBlack] forState:UIControlStateNormal];
     [acceptButton setTitle:NSLocalizedString(@"AnswerNo", "") forState:UIControlStateNormal];
     [acceptButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     
     rejectButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    rejectButton.titleLabel.font = [UIFont ysMedium20];
     rejectButton.translatesAutoresizingMaskIntoConstraints = NO;
+    rejectButton.layer.cornerRadius = 15;
+    rejectButton.titleLabel.font = [UIFont ysMedium20];
+    [rejectButton setBackgroundColor:[UIColor ypWhite]];
+    [rejectButton setTitleColor:[UIColor ypBlack] forState:UIControlStateNormal];
     [rejectButton setTitle:NSLocalizedString(@"AnswerYes", "") forState:UIControlStateNormal];
     [rejectButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     
     posterImageView = [UIImageView new];
-    posterImageView.backgroundColor = [UIColor blueColor];
     posterImageView.translatesAutoresizingMaskIntoConstraints = NO;
     posterImageView.layer.cornerRadius = 20;
+    posterImageView.backgroundColor = [UIColor blueColor];
        
     UIStackView *parentStackView = [UIStackView new];
     parentStackView.translatesAutoresizingMaskIntoConstraints = NO;
     parentStackView.axis = UILayoutConstraintAxisVertical;
-    parentStackView.spacing = 20;
+    parentStackView.spacing = 5;
     parentStackView.distribution = UIStackViewDistributionFill;
     parentStackView.alignment = UIStackViewAlignmentFill;
     
@@ -74,33 +82,37 @@
     headerStackView.axis = UILayoutConstraintAxisHorizontal;
     headerStackView.distribution = UIStackViewDistributionFill;
     headerStackView.alignment = UIStackViewAlignmentFill;
-    headerStackView.backgroundColor = [UIColor ypGreen];
     
     UIStackView *footerStackView = [UIStackView new];
     footerStackView.translatesAutoresizingMaskIntoConstraints = NO;
     footerStackView.axis = UILayoutConstraintAxisHorizontal;
-    footerStackView.distribution = UIStackViewDistributionFillProportionally;
+    footerStackView.spacing = 10;
+    footerStackView.distribution = UIStackViewDistributionFillEqually;
     
     [headerStackView addArrangedSubview:questionHeaderLabel];
     [headerStackView addArrangedSubview:questionCounterLabel];
-    
+    [headerStackView setLayoutMarginsRelativeArrangement:YES];
+    [headerStackView setLayoutMargins:UIEdgeInsetsMake(5, 0, 0, 0)];
+            
     [footerStackView addArrangedSubview:acceptButton];
     [footerStackView addArrangedSubview:rejectButton];
+    [footerStackView setLayoutMarginsRelativeArrangement:YES];
+    [footerStackView setLayoutMargins:UIEdgeInsetsMake(0, 0, 10, 0)];
     
-    UILayoutGuide *margin = self.view.layoutMarginsGuide;
-                
+    UILayoutGuide *guide = self.view.layoutMarginsGuide;
+    
     [parentStackView addArrangedSubview:headerStackView];
     [parentStackView addArrangedSubview:posterImageView];
     [parentStackView addArrangedSubview:questionRatingLabel];
     [parentStackView addArrangedSubview:footerStackView];
     [self.view addSubview:parentStackView];
-        
+                
     [posterImageView.heightAnchor constraintEqualToAnchor:posterImageView.widthAnchor multiplier:3.0/2.0].active = YES;
-    [parentStackView.leadingAnchor constraintEqualToAnchor:margin.leadingAnchor].active = YES;
-    [parentStackView.trailingAnchor constraintEqualToAnchor:margin.trailingAnchor].active = YES;
-    [parentStackView.topAnchor constraintEqualToAnchor:margin.topAnchor].active = YES;
-    [parentStackView.bottomAnchor constraintEqualToAnchor:margin.bottomAnchor].active = YES;
-    //    [footerStackView.heightAnchor constraintEqualToConstant:60].active = YES;
+    [footerStackView.heightAnchor constraintEqualToConstant:60].active = YES;
+    [parentStackView.leadingAnchor constraintEqualToAnchor:guide.leadingAnchor].active = YES;
+    [parentStackView.trailingAnchor constraintEqualToAnchor:guide.trailingAnchor].active = YES;
+    [parentStackView.topAnchor constraintEqualToAnchor:guide.topAnchor].active = YES;
+    [parentStackView.bottomAnchor constraintEqualToAnchor:guide.bottomAnchor].active = YES;
 }
 
 - (void)viewDidLayoutSubviews {
